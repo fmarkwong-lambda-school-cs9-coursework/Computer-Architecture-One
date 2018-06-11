dd4fc549f70eaf4f7a2e6a34836396884c7a9c98
Mark Wong <fmarkwong@gmail.com>
2018-06-11 15:43:16 -0700

    refactor

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -18,10 +18,11 @@ class CPU {
         // Special-purpose registers
         this.PC = 0; // Program Counter
 
-        this.instructionRunner = [];
-        this.instructionRunner[153] = this.LDI.bind(this);
-        this.instructionRunner[67] = this.PRN.bind(this);
-        this.instructionRunner[1] = this.HLT.bind(this);
+        this.instructionRunner = {
+          0b10011001: this.LDI.bind(this),
+          0b01000011: this.PRN.bind(this),
+          0b00000001: this.HLT.bind(this),
+        };
     }
     
     /**
@@ -76,48 +77,31 @@ class CPU {
         // index into memory of the instruction that's about to be executed
         // right now.)
 
-        // !!! IMPLEMENT ME
         let IR = this.ram.read(this.PC);
-
-        // Debugging output
-        // console.log(`${this.PC}: ${IR.toString(2)}`);
-        // console.log('number of operands', this.getNumberOfOperands(IR));
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
 
-        // !!! IMPLEMENT ME
         let operandA = this.ram.read(this.PC + 1);
         let operandB = this.ram.read(this.PC + 2);
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
-        // !!! IMPLEMENT ME
         this.instructionRunner[IR](operandA, operandB);
 
         // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
-        
-        // !!! IMPLEMENT ME
-      // console.log('ram is', this.ram);
         this.PC += this.getNumberOfOperands(IR) + 1;
-        // console.log('PC is', this.PC);
     }
 
+    // get value of the 2 high bits
     getNumberOfOperands(instructionRegister) {
-      const toBinary = this.decimalTo8Bit(instructionRegister); 
-      const highBits = toBinary.slice(0, 2);
-
-      return parseInt(highBits, 2);
+      return instructionRegister >> 6;
     }
 
     // Set the value of a register to an integer.
     LDI(register, integerValue) {
       this.reg[register] = integerValue;
-      // this.storeRegister(register, integerValue);    
     }
 
     PRN(register) {
@@ -128,15 +112,6 @@ class CPU {
       console.log('finis!');
       this.stopClock();
     }
-
-    // https://stackoverflow.com/questions/24337260/javascript-a-byte-is-supposed-to-be-8-bits
-    decimalTo8Bit(decimal) {
-      if (decimal < 0 || decimal > 255 || decimal % 1 !== 0) {
-        throw new Error(decimal + " does not fit in a byte");
-      }
-      return ("000000000" + decimal.toString(2)).substr(-8)
-    }
-
 }
 
 module.exports = CPU;
